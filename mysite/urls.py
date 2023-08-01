67f310dbfc5b36dd7a92d8baf152468b6c8cbbf4
aluko123 <oaa0008@uah.edu>
2023-07-31 22:29:15 -0500

    Developed REST API using tasty-pie

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from api.resources import NoteResource
+
+note_resource = NoteResource()
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('new_blog.urls')),
+    path("api/", include(note_resource.urls)),
 ]
